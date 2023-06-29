0fa12808bfdfc45c89f660b74d25db56177f0526
David Montoya <deividelpaisa@hotmail.com>
2023-06-28 23:31:02 -0500

    refactor: [T-0] 💡 added login route

--- frontend/web/src/routes/index.tsx
+++ frontend/web/src/routes/index.tsx
@@ -1,18 +1,36 @@
-import { component$ } from '@builder.io/qwik';
+import { component$, useSignal } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const pokemonId = useSignal(1);
+
   return (
     <>
-      <div>
-        <h1>Hola mundo</h1>
+      <div class="flex flex-col gap-4 justify-center items-center">
+        <span class="text-2xl">Buscador simple</span>
+        <span class="text-9xl">{pokemonId}</span>
+
+        <div class="flex flex-row gap-4">
+          <button
+            onClick$={() => pokemonId.value--}
+            class="font-bold py-2 px-4 rounded bg-purple-600"
+          >
+            Anterior
+          </button>
+          <button
+            onClick$={() => pokemonId.value++}
+            class="font-bold py-2 px-4 rounded bg-red-500"
+          >
+            Siguiente
+          </button>
+        </div>
       </div>
     </>
-  )
+  );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: 'Boilerplate NX',
   meta: [
     {
       name: 'description',
