e7a620168fe6673df526b1d2e621df2121231658
David Montoya <deividelpaisa@hotmail.com>
2023-06-30 19:39:28 -0500

    refactor: [T-0] 💡 structured component structure

--- frontend/ui/.storybook/main.ts
+++ frontend/ui/.storybook/main.ts
@@ -1,9 +1,12 @@
-import { mergeConfig, UserConfig } from 'vite';
 import { withNx } from 'qwik-nx/storybook';
+import { mergeConfig, UserConfig } from 'vite';
 import viteConfig from './../vite.config';
 
 const config = {
-  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: ['@storybook/addon-essentials'],
   framework: { name: 'storybook-framework-qwik' },
   async viteFinal(config: UserConfig) {
