b3c3b0ea4b729144235e4473784dc0feaf34172a
David Montoya <deividelpaisa@hotmail.com>
2023-07-03 14:40:15 -0500

    feat: [T-0] 🔥 dashboard route protection implemented

--- frontend/web/src/routes/dashboard/layout.tsx
+++ frontend/web/src/routes/dashboard/layout.tsx
@@ -1,9 +1,21 @@
 import { Slot, component$ } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+export const useCheckAuthCookie = routeLoader$(({ cookie, redirect }) => {
+  const jwtCookie = cookie.get('jwt');
+
+  if (jwtCookie) {
+    console.log('Cookie value:', jwtCookie);
+    return;
+  }
+
+  redirect(302, '/login');
+});
 
 export default component$(() => {
   return (
     <>
-      <h3>Dashboard Layout</h3>
+      <h3>Dashboard</h3>
       <Slot />
     </>
   );
