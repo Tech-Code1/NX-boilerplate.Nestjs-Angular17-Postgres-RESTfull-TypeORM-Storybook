5dd6f85d4982338cf451abd7062cc3ba56ee2199
David Montoya <deividelpaisa@hotmail.com>
2023-07-12 20:27:33 -0500

    style: [T-0] 🎨 button login and register change style

--- frontend/web/src/routes/(auth)/(login)/layout.tsx
+++ frontend/web/src/routes/(auth)/(login)/layout.tsx
@@ -1,11 +1,37 @@
 import { component$, Slot } from '@builder.io/qwik';
-import { Link } from '@builder.io/qwik-city';
+import { Link, useLocation } from '@builder.io/qwik-city';
 
 export default component$(() => {
+  const route = useLocation();
+
   return (
-    <div class="flex flex-col items-center justify-center w-full h-full p-4">
-      <Link href="/login">login</Link>
-      <Link href="/register">register</Link>
+    <div class="flex flex-row items-center justify-center w-full h-full gap-8">
+      <div class="flex flex-col gap-4">
+        <Link
+          class={[
+            {
+              'flex justify-center p-4 bg-purple-300 rounded-md font-bold text-purple-700':
+                route.url.pathname === '/login/',
+            },
+            'flex justify-center p-4 bg-gray-100 rounded-md',
+          ]}
+          href="/login"
+        >
+          Login
+        </Link>
+        <Link
+          class={[
+            {
+              'flex justify-center p-4 bg-purple-300 rounded-md font-bold text-purple-700':
+                route.url.pathname === '/register/',
+            },
+            'flex justify-center p-4 bg-gray-100 rounded-md',
+          ]}
+          href="/register"
+        >
+          Register
+        </Link>
+      </div>
       <Slot />
     </div>
   );
