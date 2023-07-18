10ff179dd463cefb1971cfa399cf5eef02115270
David Montoya <deividelpaisa@hotmail.com>
2023-07-17 20:46:07 -0500

    style: [T-0] 🎨 twailwind config added

---
+++ frontend/ui/tailwind.config.js
@@ -0,0 +1,11 @@
+const { createGlobPatternsForDependencies } = require('@nx/angular/tailwind');
+const { join } = require('path');
+const sharedTailwindConfig = require('../../libs/tailwind-preset/tailwind.config');
+
+module.exports = {
+  presets: [sharedTailwindConfig],
+  content: [
+    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+};
---
+++ libs/tailwind-preset/tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  theme: {
+    colors: {
+      primary: {
+        light: '#5eead4',
+        DEFAULT: '#14b8a6',
+        dark: '#0f766e',
+      },
+      secondary: {
+        light: '#bae6fd',
+        DEFAULT: '#0ea5e9',
+        dark: '#0369a1',
+      },
+      white: '#ffffff',
+      black: '#000000',
+    },
+    spacing: {
+      sm: '0.5rem',
+      md: '1rem',
+      lg: '1.5rem',
+      xl: '2rem',
+    },
+  },
+  plugins: [],
+};
